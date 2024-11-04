2ff7682ddef27648756452cb6534a01f41b01aa8
stephenw02 <smwatkins02@gmail.com>
2024-11-04 17:01:25 -0500

    Env vars updated

--- baltparkweb/settings.py
+++ baltparkweb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
